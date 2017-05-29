ab61a82f94fccb92e412e364801eb16c5463c72c
natanaela <natanaela_stana2007@yahoo.com>
2017-05-29 22:06:17 +0300

    schimba col cu row in step 3

--- StepsForGUI/src/ImportImage/Step3.java
+++ StepsForGUI/src/ImportImage/Step3.java
@@ -17,8 +17,8 @@ class Step3 {
     static double[][] run(double[][] step2Result) {
          ArrayList<Double[]>  temp;
         temp = analysis(step2Result);
-        temp = keepAreaUnderThreshold(temp, 100);
-        
+        temp = keepWithAreaUnderThreshold(temp, 100);
+        int distinctRow = countDistinctRow(temp);
         double[][] toReturn = null;
         return toReturn;
 
@@ -29,23 +29,23 @@ class Step3 {
         ArrayList<Double[]> c = new ArrayList<>();
         int width = copyPoza.length;
         int height = copyPoza[0].length;
-        Integer ci, cj, wi, wj;
+        Integer colColtSt, rowColtSus, latimeCol, latimeRow;
             log.println("Matricea C dupa analysis");
        
             for (int row = 0; row < height; row++) {
                  for (int col = 0; col < width; col++) {
                 if (copyPoza[col][row] == 0){
                     if(copyPoza[col+1][row]==0 &&copyPoza[col][row+1]==0){//coltul  
-                        ci=col;
-                        cj=row;
-                        wi=0;
-                        while(copyPoza[col+wi][row]==0)
-                            wi=wi+1;
-                        wj=0;
-                        while(copyPoza[col][row+wj]==0)
-                            wj=wj+1;
-                        Double[] var = new Double[]{ci.doubleValue(),cj.doubleValue(),wi.doubleValue(),wj.doubleValue()};
-                        log.println(String.format("%d, %d, %d, %d", ci,cj,wi,wj));
+                        colColtSt=col;
+                        rowColtSus=row;
+                        latimeCol=0;
+                        while(copyPoza[col+latimeCol][row]==0)
+                            latimeCol=latimeCol+1;
+                        latimeRow=0;
+                        while(copyPoza[col][row+latimeRow]==0)
+                            latimeRow=latimeRow+1;
+                        Double[] var = new Double[]{rowColtSus.doubleValue(),colColtSt.doubleValue(),latimeRow.doubleValue(),latimeCol.doubleValue()};
+                        log.println(String.format("%d, %d, %d, %d", rowColtSus,colColtSt,latimeRow,latimeCol));
                         c.add(var);
                     }
                 }
@@ -55,8 +55,12 @@ class Step3 {
 
     }
 
-    private static ArrayList<Double[]> keepAreaUnderThreshold( ArrayList<Double[]> aux, int i) {
+    private static ArrayList<Double[]> keepWithAreaUnderThreshold( ArrayList<Double[]> aux, int i) {
 return null;
+    }
+
+    private static int countDistinctRow(ArrayList<Double[]> temp) {
+        return 0;
     }
 
 }
