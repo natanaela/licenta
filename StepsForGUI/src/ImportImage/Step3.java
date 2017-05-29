0e2515e5998c6833b4149d5abb5f52990e70175a
natanaela <natanaela_stana2007@yahoo.com>
2017-05-29 21:19:39 +0300

    update step3 logic

--- StepsForGUI/src/ImportImage/Step3.java
+++ StepsForGUI/src/ImportImage/Step3.java
@@ -14,40 +14,48 @@ import java.util.ArrayList;
 class Step3 {
 
     static double[][] run(double[][] step2Result) {
-        double[][] toReturn;
-        toReturn = analysis(step2Result);
-
+         ArrayList<Double[]>  temp;
+        temp = analysis(step2Result);
+        temp = keepAreaUnderThreshold(temp, 100);
+        
+        double[][] toReturn = null;
         return toReturn;
 
     }
 
-    private static double[][] analysis(double[][] step2Result) {
+    private static  ArrayList<Double[]> analysis(double[][] step2Result) {
         double[][] copyPoza = step2Result.clone();
         ArrayList<Double[]> c = new ArrayList<>();
         int width = copyPoza.length;
         int height = copyPoza[0].length;
-        double ci, cj, wi, wj;
+        Integer ci, cj, wi, wj;
         
-        for (int col = 0; col < width; col++) {
+       
             for (int row = 0; row < height; row++) {
+                 for (int col = 0; col < width; col++) {
                 if (copyPoza[col][row] == 0){
-                    if(copyPoza[col+1][row]==0 &&copyPoza[col][row+1]==0){//coltul
+                    if(copyPoza[col+1][row]==0 &&copyPoza[col][row+1]==0){//coltul  
                         ci=col;
                         cj=row;
                         wi=0;
-                        while(copyPoza[col+(int)wi][row]==0)
+                        while(copyPoza[col+wi][row]==0)
                             wi=wi+1;
                         wj=0;
-                        while(copyPoza[col][row+(int)wj]==0)
+                        while(copyPoza[col][row+wj]==0)
                             wj=wj+1;
-                        Double[] var = new Double[]{ci,cj,wi,wj};
+                        Double[] var = new Double[]{ci.doubleValue(),cj.doubleValue(),wi.doubleValue(),wj.doubleValue()};
+                        System.out.println(String.format("%f, %f, %f, %f", var[0],var[1],var[2],var[3]));
                         c.add(var);
                     }
                 }
             }
         }
-        return null;
+        return c;
 
+    }
+
+    private static ArrayList<Double[]> keepAreaUnderThreshold( ArrayList<Double[]> aux, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
